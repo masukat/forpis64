f280a17a39740377f15601201f5558108acb49fd
asuka <asuka@ksuzuki.local>
2020-04-17 00:06:37 +0900

    20200416_setting_Debug問題

--- my_app/settings.py
+++ my_app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['forpis64.herokuapp.com','127.0.0.1','localhost']
 
@@ -133,7 +133,7 @@ print("DEBUG")
 print(DEBUG)
 
 # This is new:heroku postgres
-if DEBUG:
+if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
