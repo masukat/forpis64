fe0ce4e64f0b3bece6e513bda73872b7c5ae1ce3
asuka <asuka@ksuzuki.local>
2020-04-16 23:27:29 +0900

    20200416_setting_DB接続できない問題修正_debug=true,print DEBUG

--- my_app/settings.py
+++ my_app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['forpis64.herokuapp.com','127.0.0.1','localhost']
 
@@ -129,6 +129,8 @@ try:
 except ImportError:
     pass
 
+print("DEBUG")
+print(DEBUG)
 # This is new:heroku postgres
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
