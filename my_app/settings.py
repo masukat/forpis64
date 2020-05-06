177edbc5fe054a71e0d28f49e245f97faf56c781
asuka <asuka@ksuzuki.local>
2020-05-06 09:03:28 +0900

    20200506_setting debug=false,if not変更

--- my_app/settings.py
+++ my_app/settings.py
@@ -130,7 +130,7 @@ except ImportError:
     pass
 
 # This is new:heroku postgres
-if DEBUG:
+if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
