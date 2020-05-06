fa7c4de5e6ff800cb4b153781fd6acfbefa5b10b
asuka <asuka@ksuzuki.local>
2020-05-06 12:52:32 +0900

    20200506_setting debug=true,if debug変更

--- my_app/settings.py
+++ my_app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['forpis64.herokuapp.com','127.0.0.1','localhost']
 
@@ -119,21 +119,13 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'forpis/static'),
 )
 
-print('DEBUG')
-print(DEBUG)
-
 try:
     from .local_settings import *
 except ImportError:
     pass
-    print('localimportError')
 
-print('try後')
 # This is new:heroku postgres
-if not DEBUG:
-    print('if not DEBUG:の中')
-    print('DEBUG')
-    print(DEBUG)
+if DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
