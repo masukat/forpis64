7637c8046e58bf239e8d2839822d5dff15d94181
asuka <asuka@ksuzuki.local>
2020-06-12 20:40:59 +0900

    20200612_views変更

--- forpis/views/progress_views.py
+++ forpis/views/progress_views.py
@@ -78,15 +78,18 @@ def progress_relate_delete(request,num):
         }
     return render(request, 'forpis/progress/relate.html', params)
 
+
+
+
+
+
 def progress_habit(request):
+    data_problem = Habit.objects.all().order_by('probgenre1','probgenre2' ,'updated_at').reverse()
     data_habit = Habit.objects.all().order_by('motivation','frequency','division','habitgenre1').reverse()
-    data_undone = Habit.objects.all().order_by('motivation','habitgenre1').reverse()
-    data_done = Habit.objects.all().order_by('completiondate','review').reverse()
 
     params = {
+        'data_problem':data_problem,
         'data_habit':data_habit,
-        'data_undone':data_undone,
-        'data_done':data_done,
         'form':HabitForm(),
         'form_motivation':HabitForm_motivation(),
         'form_frequency':HabitForm_frequency(),
@@ -111,9 +114,8 @@ def progress_habit(request):
     return render(request, 'forpis/progress/habit.html', params)
 
 def progress_habit_edit(request,num):
+    data_problem = Habit.objects.all().order_by('probgenre1','probgenre2' ,'updated_at').reverse()
     data_habit = Habit.objects.all().order_by('motivation','frequency','division','habitgenre1').reverse()
-    data_undone = Habit.objects.all().order_by('motivation','habitgenre1').reverse()
-    data_done = Habit.objects.all().order_by('completiondate','review').reverse()
 
     obj = Habit.objects.get(id=num)
 
@@ -130,9 +132,8 @@ def progress_habit_edit(request,num):
         habitgenre1.save()
         return redirect(to='/forpis/progress/habit')
     params = {
+        'data_problem':data_problem,
         'data_habit':data_habit,
-        'data_undone':data_undone,
-        'data_done':data_done,
         'form':HabitForm(instance=obj),
         'form_motivation':HabitForm_motivation(instance=obj),
         'form_frequency':HabitForm_frequency(instance=obj),
@@ -144,18 +145,16 @@ def progress_habit_edit(request,num):
     return render(request, 'forpis/progress/habit.html', params)
 
 def progress_habit_delete(request,num):
+    data_problem = Habit.objects.all().order_by('probgenre1','probgenre2' ,'updated_at').reverse()
     data_habit = Habit.objects.all().order_by('motivation','frequency','division','habitgenre1').reverse()
-    data_undone = Habit.objects.all().order_by('motivation','habitgenre1').reverse()
-    data_done = Habit.objects.all().order_by('completiondate','review').reverse()
 
     habit = Habit.objects.get(id=num)
     if (request.method == 'POST'):
         habit.delete()
         return redirect(to='/forpis/progress/habit')
     params = {
+        'data_problem':data_problem,
         'data_habit':data_habit,
-        'data_undone':data_undone,
-        'data_done':data_done,
         'obj':habit,
         'id':num,
         'delete_flag':1,
